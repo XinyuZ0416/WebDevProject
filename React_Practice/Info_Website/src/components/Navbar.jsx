61f3554c63eacafb7067024aa0da8b964eb87795
Xinyu Z <syuxxxsp@163.com>
2024-01-25 18:27:16 +0000

    upload Info_Website

---
+++ React_Practice/Info_Website/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+// import './Navbar.css'
+
+export default function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="src/assets/react.svg"/>
+                <h3>ReactFacts</h3>
+                <h4>React Course - Project 1</h4>
+            </nav>
+        </header>
+    )
+}
