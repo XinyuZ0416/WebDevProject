4cb2071fb10ed2aa57573dc74988cdbeaff13feb
Xinyu Z <syuxxxsp@163.com>
2024-01-31 20:55:07 +0000

    implemented cards, deployed

---
+++ React_Practice/AirBnb_Experience/README.md
@@ -0,0 +1,3 @@
+React + Vite
+Deployed with netlify
+Find the result at https://65bab31348c2cd1d2641b129--thunderous-cobbler-5e9502.netlify.app
---
+++ React_Practice/AirBnb_Experience/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return(
+        <>
+            <nav>
+                <img src="/airbnb-logo.png" className="nav--logo"/>
+            </nav>
+            
+        </> 
+    )
+}
