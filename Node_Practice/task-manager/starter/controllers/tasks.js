79f9b301a6bc1efec1b015bf52a49e7dd4372377
Xinyu Z <syuxxxsp@163.com>
2024-02-01 19:20:23 +0000

    implemented mongoose schema

--- Node_Practice/task-manager/starter/controllers/tasks.js
+++ Node_Practice/task-manager/starter/controllers/tasks.js
@@ -1,9 +1,13 @@
+const Task = require('../models/Task.js')
+
 const getAllTasks = (req,res) => res.send('get all tasks')
 
 const getTask = (req, res) => res.json({id: req.params.id})
 
-const createTask = (req, res) => res.json(req.body)
-
+const createTask = async(req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json(task)
+}
 const updateTask = (req, res) => res.send('update one task')
 
 const deleteTask = (req, res) => res.send('delete one task')
