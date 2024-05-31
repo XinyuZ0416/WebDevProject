565ba70b5c844dd9677ea667c7f54f594be50d58
Xinyu <Xinyu@eduda96.kent.ac.uk>
2024-05-31 11:40:34 +0100

    enabled adding texts on local memesData.js

---
+++ React_Practice/meme-generator/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header';
+import Meme from './Meme';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Meme />
+    </>
+  );
+}
+
+export default App;
